d172792b2a98956cd11cbd79cd687e541c33b022
Der-Eddy <dereddyteddy@gmail.com>
2018-11-03 15:38:45 +0100

    Added 404 error page

--- flask/main.py
+++ flask/main.py
@@ -34,3 +34,7 @@ def api():
     }
     requests.post('https://piwik.eddy-dev.net/piwik.php', data=data)
     return jsonify(jsonData)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', data=jsonData, gitHash=sha), 404
